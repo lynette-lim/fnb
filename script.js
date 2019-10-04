6619ab97a7ce9954ad1eec5eb1d4c3ee3a0bfa51
Ubuntu <ubuntu@ip-172-31-33-33.ec2.internal>
2019-10-04 15:44:41 +0000

    Optimized search terms and added more details to MapBox Popup

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 /* global axios */
 /* global $*/
+/* global mapboxgl */
+/* global venue */
 
 // define frequently used constants
 const API_URL = 'https://api.foursquare.com/v2';
@@ -27,76 +29,76 @@ let types = {
 
 //Cuisines
 let cuisines = {
-'Bakery':'bakery',
-'Cafe':'cafe café',
-'Fast food':'fast food',
-'Hawker Centre':'hawkercentre',
-'Pub & Bar':'pub bar',
-'Restaurant':'restaurant',
-'Japanese':'Japanese',
-'Chinese':'Chinese',
-'Asian Fusion':'Asian Fusion',
-'Singaporean':'Singaporean',
-'Indian':'Indian',
-'Thai':'Thai',
-'Korean':'Korean',
-'Cantonese':'Cantonese',
-'Indonesian':'Indonesian',
-'Vietnamese':'Vietnamese',
-'Szechuan':'Szechuan',
-'Malaysian':'Malaysian',
-'Taiwanese':'Taiwanese',
-'Shanghainese':'Shanghainese',
-'Italian':'Italian',
-'American':'American',
-'French':'French',
-'European':'European',
-'Middle Eastern':'Middle Eastern',
-'Mediterranean':'Mediterranean',
-'Mexican':'Mexican',
-'Spanish':'Spanish',
-'Turkish':'Turkish',
-'German':'German',
-'Lebanese':'Lebanese',
-'Wine':'Wine',
-'Beer':'Beer',
-'Cocktail':'Cocktail',
-'Coffee':'Coffee',
-'Tea':'Tea',
-'Juice':'Juice',
-'Seafood':'Seafood',
-'Sushi':'Sushi',
-'Breakfast':'Breakfast',
-'Brunch':'Brunch',
-'Ramen':'Ramen',
-'Dessert':'Dessert',
-'Salad':'Salad',
-'Dim Sum':'Dim Sum',
-'Pizza':'Pizza',
-'Burgers':'Burgers',
-'Sandwiches':'Sandwiches',
-'Dessert':'Dessert',
-'IceCream':'IceCream',
-'Frozen Yogurt':'Frozen Yogurt',
-'Soup':'Soup',
-'Buffet':'Buffet',
-'Steakhouse':'Steakhouse',
-'Barbeque':'Barbeque',
-'Noodles':'Noodles',
-'Bubble Tea':'Bubble Tea',
-'Tapas/Small Plates':'Tapas',
-'HotPot':'HotPot',
-'Izakaya':'Izakaya',
-'Chicken':'Chicken',
-'Fish & Chips':'Fish & Chips',
-'Kebab':'Kebab',
-'Patisserie':'Patisserie',
-'Cake':'Cake',
-'Chicken Wings':'Chicken Wings',
-'Creperies':'Creperies',
-'Vegan':'Vegan',
-'Vegetarian':'Vegetarian',
-'Halal':'Halal'
+  'Bakery': 'bakery',
+  'Café': 'cafe',
+  'Fast food': 'fast food',
+  'Hawker Centre': 'hawkercentre',
+  'Pub & Bar': 'pub bar',
+  'Restaurant': 'restaurant',
+  'Japanese': 'Japanese',
+  'Chinese': 'Chinese',
+  'Asian Fusion': 'Asian Fusion',
+  'Singaporean': 'Singaporean',
+  'Indian': 'Indian',
+  'Thai': 'Thai',
+  'Korean': 'Korean',
+  'Cantonese': 'Cantonese',
+  'Indonesian': 'Indonesian',
+  'Vietnamese': 'Vietnamese',
+  'Szechuan': 'Szechuan',
+  'Malaysian': 'Malaysian',
+  'Taiwanese': 'Taiwanese',
+  'Shanghainese': 'Shanghainese',
+  'Italian': 'Italian',
+  'American': 'American',
+  'French': 'French',
+  'European': 'European',
+  'Middle Eastern': 'Middle Eastern',
+  'Mediterranean': 'Mediterranean',
+  'Mexican': 'Mexican',
+  'Spanish': 'Spanish',
+  'Turkish': 'Turkish',
+  'German': 'German',
+  'Lebanese': 'Lebanese',
+  'Wine': 'Wine',
+  'Beer': 'Beer',
+  'Cocktail': 'Cocktail',
+  'Coffee': 'Coffee',
+  'Tea': 'Tea',
+  'Juice': 'Juice',
+  'Seafood': 'Seafood',
+  'Sushi': 'Sushi',
+  'Breakfast': 'Breakfast',
+  'Brunch': 'Brunch',
+  'Ramen': 'Ramen',
+  'Dessert': 'Dessert',
+  'Salad': 'Salad',
+  'Dim Sum': 'Dim Sum',
+  'Pizza': 'Pizza',
+  'Burgers': 'Burgers',
+  'Sandwiches': 'Sandwiches',
+  'Dessert': 'Dessert',
+  'IceCream': 'IceCream',
+  'Frozen Yogurt': 'Frozen Yogurt',
+  'Soup': 'Soup',
+  'Buffet': 'Buffet',
+  'Steakhouse': 'Steakhouse',
+  'Barbeque': 'Barbeque',
+  'Noodles': 'Noodles',
+  'Bubble Tea': 'Bubble Tea',
+  'Tapas/Small Plates': 'Tapas',
+  'HotPot': 'HotPot',
+  'Izakaya': 'Izakaya',
+  'Chicken': 'Chicken',
+  'Fish & Chips': 'Fish & Chips',
+  'Kebab': 'Kebab',
+  'Patisserie': 'Patisserie',
+  'Cake': 'Cake',
+  'Chicken Wings': 'Chicken Wings',
+  'Creperies': 'Creperies',
+  'Vegan': 'Vegan',
+  'Vegetarian': 'Vegetarian',
+  'Halal': 'Halal'
 }
 
 
@@ -118,7 +120,7 @@ function testGetFourSquare() {
       "query": '', // what we are searching for
       "categoryId": '4d4b7105d754a06374d81259, 4bf58dd8d48988d16e941735, 4bf58dd8d48988d120951735, 4bf58dd8d48988d16d941735, 4bf58dd8d48988d128941735'
     }
-  }).then(function (response) {
+  }).then(function(response) {
     console.log(response.data.response.groups[0].items);
   })
 }
@@ -130,20 +132,20 @@ let all_markers = [];
 //set maxBounds for Map
 let bounds = [
   [103.599921, 1.212660], // Southwest coordinates
-  [104.044757, 1.489435]  // Northeast coordinates
+  [104.044757, 1.489435] // Northeast coordinates
 ];
 
 //AUTOCOMPLETE UI
-$(function () {
+$(function() {
   $.widget("custom.catcomplete", $.ui.autocomplete, {
-    _create: function () {
+    _create: function() {
       this._super();
       this.widget().menu("option", "items", "> :not(.ui-autocomplete-category)");
     },
-    _renderMenu: function (ul, items) {
+    _renderMenu: function(ul, items) {
       var that = this,
         currentCategory = "";
-      $.each(items, function (index, item) {
+      $.each(items, function(index, item) {
         var li;
         if (item.category != currentCategory) {
           ul.append("<li class='ui-autocomplete-category'>" + item.category + "</li>");
@@ -157,84 +159,84 @@ $(function () {
     }
   });
   var data = [
-{label:"Bakery", category:"Establishment Type"},
-{label:"Café", category:"Establishment Type"},
-{label:"Fast food", category:"Establishment Type"},
-{label:"Food Court", category:"Establishment Type"},
-{label:"Hawker Centre", category:"Establishment Type"},
-//test pub if PUB East
-{label:"Pub & Bar", category:"Establishment Type"},
-{label:"Restaurant", category:"Establishment Type"},
-{label:"Japanese",category:"Asian"},
-{label:"Chinese",category:"Asian"},
-{label:"Asian Fusion",category:"Asian"},
-{label:"Singaporean",category:"Asian"},
-{label:"Indian",category:"Asian"},
-{label:"Thai",category:"Asian"},
-{label:"Korean",category:"Asian"},
-{label:"Cantonese",category:"Asian"},
-{label:"Indonesian",category:"Asian"},
-{label:"Vietnamese",category:"Asian"},
-{label:"Szechuan",category:"Asian"},
-{label:"Malaysian",category:"Asian"},
-{label:"Taiwanese",category:"Asian"},
-{label:"Shanghainese",category:"Asian"},
-{label:"Italian",category:"International"},
-{label:"American",category:"International"},
-{label:"French",category:"International"},
-{label:"European",category:"International"},
-{label:"Middle Eastern",category:"International"},
-{label:"Mediterranean",category:"International"},
-{label:"Mexican",category:"International"},
-{label:"Spanish",category:"International"},
-{label:"Turkish",category:"International"},
-{label:"German",category:"International"},
-{label:"Lebanese",category:"International"},
-{label:"Wine",category:"Beverages"},
-{label:"Beer",category:"Beverages"},
-{label:"Cocktail",category:"Beverages"},
-{label:"Coffee",category:"Beverages"},
-{label:"Tea",category:"Beverages"},
-{label:"Juice",category:"Beverages"},
-{label:"Seafood",category:"Food"},
-{label:"Sushi",category:"Food"},
-{label:"Breakfast",category:"Food"},
-{label:"Brunch",category:"Food"},
-{label:"Ramen",category:"Food"},
-{label:"Dessert",category:"Food"},
-{label:"Salad",category:"Food"},
-{label:"Dim Sum",category:"Food"},
-{label:"Pizza",category:"Food"},
-{label:"Burgers",category:"Food"},
-{label:"Sandwiches",category:"Food"},
-{label:"Dessert",category:"Food"},
-{label:"Ice Cream",category:"Food"},
-{label:"Frozen Yogurt",category:"Food"},
-{label:"Soup",category:"Food"},
-{label:"Buffet",category:"Food"},
-{label:"Steakhouse",category:"Food"},
-{label:"Barbeque",category:"Food"},
-{label:"Noodles",category:"Food"},
-{label:"Bubble Tea",category:"Food"},
-{label:"Tapas/Small Plates",category:"Food"},
-{label:"Hot Pot",category:"Food"},
-{label:"Izakaya",category:"Food"},
-{label:"Chicken",category:"Food"},
-{label:"Fish & Chips",category:"Food"},
-{label:"Kebab",category:"Food"},
-{label:"Patisserie",category:"Food"},
-{label:"Cake",category:"Food"},
-{label:"Chicken Wings",category:"Food"},
-{label:"Creperies",category:"Food"},
-{label:"Vegan",category:"Dietary Options"},
-{label:"Vegetarian",category:"Dietary Options"},
-{label:"Halal",category:"Dietary Options"}
+    { label: "Bakery", category: "Establishment Type" },
+    { label: "Café", category: "Establishment Type" },
+    { label: "Fast food", category: "Establishment Type" },
+    { label: "Food Court", category: "Establishment Type" },
+    { label: "Hawker Centre", category: "Establishment Type" },
+    //test pub if PUB East
+    { label: "Pub & Bar", category: "Establishment Type" },
+    { label: "Restaurant", category: "Establishment Type" },
+    { label: "Japanese", category: "Asian" },
+    { label: "Chinese", category: "Asian" },
+    { label: "Asian Fusion", category: "Asian" },
+    { label: "Singaporean", category: "Asian" },
+    { label: "Indian", category: "Asian" },
+    { label: "Thai", category: "Asian" },
+    { label: "Korean", category: "Asian" },
+    { label: "Cantonese", category: "Asian" },
+    { label: "Indonesian", category: "Asian" },
+    { label: "Vietnamese", category: "Asian" },
+    { label: "Szechuan", category: "Asian" },
+    { label: "Malaysian", category: "Asian" },
+    { label: "Taiwanese", category: "Asian" },
+    { label: "Shanghainese", category: "Asian" },
+    { label: "Italian", category: "International" },
+    { label: "American", category: "International" },
+    { label: "French", category: "International" },
+    { label: "European", category: "International" },
+    { label: "Middle Eastern", category: "International" },
+    { label: "Mediterranean", category: "International" },
+    { label: "Mexican", category: "International" },
+    { label: "Spanish", category: "International" },
+    { label: "Turkish", category: "International" },
+    { label: "German", category: "International" },
+    { label: "Lebanese", category: "International" },
+    { label: "Wine", category: "Beverages" },
+    { label: "Beer", category: "Beverages" },
+    { label: "Cocktail", category: "Beverages" },
+    { label: "Coffee", category: "Beverages" },
+    { label: "Tea", category: "Beverages" },
+    { label: "Juice", category: "Beverages" },
+    { label: "Seafood", category: "Food" },
+    { label: "Sushi", category: "Food" },
+    { label: "Breakfast", category: "Food" },
+    { label: "Brunch", category: "Food" },
+    { label: "Ramen", category: "Food" },
+    { label: "Dessert", category: "Food" },
+    { label: "Salad", category: "Food" },
+    { label: "Dim Sum", category: "Food" },
+    { label: "Pizza", category: "Food" },
+    { label: "Burgers", category: "Food" },
+    { label: "Sandwiches", category: "Food" },
+    { label: "Dessert", category: "Food" },
+    { label: "Ice Cream", category: "Food" },
+    { label: "Frozen Yogurt", category: "Food" },
+    { label: "Soup", category: "Food" },
+    { label: "Buffet", category: "Food" },
+    { label: "Steakhouse", category: "Food" },
+    { label: "Barbeque", category: "Food" },
+    { label: "Noodles", category: "Food" },
+    { label: "Bubble Tea", category: "Food" },
+    { label: "Tapas/Small Plates", category: "Food" },
+    { label: "Hot Pot", category: "Food" },
+    { label: "Izakaya", category: "Food" },
+    { label: "Chicken", category: "Food" },
+    { label: "Fish & Chips", category: "Food" },
+    { label: "Kebab", category: "Food" },
+    { label: "Patisserie", category: "Food" },
+    { label: "Cake", category: "Food" },
+    { label: "Chicken Wings", category: "Food" },
+    { label: "Creperies", category: "Food" },
+    { label: "Vegan", category: "Dietary Options" },
+    { label: "Vegetarian", category: "Dietary Options" },
+    { label: "Halal", category: "Dietary Options" }
   ];
 
   $("#userInputCuisine").catcomplete({
     delay: 0,
     source: data,
-    onSelect: function () {
+    onSelect: function() {
       alert("selected");
     }
   });
@@ -268,18 +270,18 @@ function getFastFood() {
       "categoryId": '4d4b7105d754a06374d81259, 4bf58dd8d48988d16e941735' //1.food | 2.fast food, 3.foodcourt, 4.cafe 5.cafeteria
 
     }
-  }).then(function (response) {
+  }).then(function(response) {
     console.log(response.data.response.groups[0].items);
   })
 }
 // ------------
 
-$(function () {
+$(function() {
   //show map
   setupMap();
 
   //To trigger Region change via Region Select HTML
-  $('#userOptionRegion').change(function () {
+  $('#userOptionRegion').change(function() {
     let region_code = $(this).val();
     console.log(regions[region_code]);
 
@@ -290,60 +292,12 @@ $(function () {
   });
 
 
-  //To trigger Type change via Type Select HTML
-  $('#userOptionType').change(function () {
-    let type_code = $(this).val();
-    let center = map.getCenter();
-    console.log(center);
-    console.log(types[type_code])
-
-    axios.get(API_URL + "/venues/explore", {
-      params: {
-        "client_id": CLIENT_ID,
-        "client_secret": CLIENT_SECRET,
-        "v": '20192609', // v for is the version
-        "limit": 50, // limit is how many results returned
-        "ll": center.lat + "," + center.lng, // latitude/longtitude
-        "query": types[type_code] // what we are searching for  
-        // "categoryId": '4d4b7105d754a06374d81259' //category: food
-      }
-      //THEN PRINT MARKERS
-    }).then(function (response) {
-      $("#results").empty();
-
-
-      for (let each_marker of all_markers) {
-        each_marker.remove();
-      }
-
-      all_markers = [];
-
-      let results = response.data.response.groups[0].items;
-      for (let each_result of results) {
-        let marker = new mapboxgl.Marker();
-        marker.setLngLat([each_result.venue.location.lng, each_result.venue.location.lat]);
-        marker.addTo(map); // <-- map is a global variable holding the mapboxgl Map object
-
-        let popup = new mapboxgl.Popup({
-          offset: 25
-        });
-
-        popup.setHTML(each_result.venue.name);
-
-        marker.setPopup(popup);
-
-        all_markers.push(marker);
-
-      }
-    })
-  })
 
 
   // To trigger Cuisine change via Cuisine Searhbox HTML
-  $('#userInputCuisine').change(function () {
+  $('#userInputCuisine').change(function() {
     let center = map.getCenter();
     cuisine_code = $('#userInputCuisine').val();
-    type_code = $('#userOptionType').val();
     console.log(cuisines[cuisine_code]);
 
     axios.get(API_URL + "/venues/explore", {
@@ -351,13 +305,13 @@ $(function () {
         "client_id": CLIENT_ID,
         "client_secret": CLIENT_SECRET,
         "v": '20192609', // v for is the version
-        "limit": 50, // limit is how many results returned
+        "limit": 20, // limit is how many results returned
         "ll": center.lat + "," + center.lng, // latitude/longtitude
-        "query": cuisines[cuisine_code] + ',' + types[type_code] // what we are searching for  
+        "query": cuisines[cuisine_code] // what we are searching for 
       }
 
       //****THROW THESE INTO A FUNCTION LATER (DECLARE FUNCTION)**
-    }).then(function (response) {
+    }).then(function(response) {
       $("#results").empty();
 
 
@@ -368,20 +322,26 @@ $(function () {
       all_markers = [];
 
       let results = response.data.response.groups[0].items;
-        for (let each_result of results) {
-          let marker = new mapboxgl.Marker();
-          marker.setLngLat([each_result.venue.location.lng, each_result.venue.location.lat]);
-          marker.addTo(map); // <-- map is a global variable holding the mapboxgl Map object
+      for (let r of results) {
+        let marker = new mapboxgl.Marker();
+        marker.setLngLat([r.venue.location.lng, r.venue.location.lat]);
+        marker.addTo(map); // <-- map is a global variable holding the mapboxgl Map object
 
-          let popup = new mapboxgl.Popup({
-            offset: 25
-          });
+        let popup = new mapboxgl.Popup({
+          offset: 10
+        });
 
-          popup.setHTML(each_result.venue.name);
+        
+        //popup content: name, icon, address, postal code
+        popup.setHTML("<b>" + r.venue.name + "</b>" +
+          "<img src=" + r.venue.categories[0].icon + "32.png>" +
+          "<p>" + r.venue.location.formattedAddress[0] +
+          "<br>Singapore (" + r.venue.location.formattedAddress[1] + ")</p>");
 
-          marker.setPopup(popup);
 
-          all_markers.push(marker);
+        marker.setPopup(popup);
+
+        all_markers.push(marker);
 
       }
     })
