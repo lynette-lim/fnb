0423e95dcb0cd62e6cb3f1d9864c9e7082052740
Ubuntu <ubuntu@ip-172-31-33-33.ec2.internal>
2019-10-04 03:19:09 +0000

    Added Mapbox and 2 selectors

--- script.js
+++ script.js
@@ -6,6 +6,25 @@ const API_URL = 'https://api.foursquare.com/v2';
 const CLIENT_ID = 'MBW5E3D455SVF2HV2BPEB2TJG21XCZTA3WDAZ4U5I4PMPLCZ';
 const CLIENT_SECRET = "ETY2MWRODHNEDLP3MGGWKFX0AM1TLAA0XHZ5HW0S1YNTL2AA";
 
+
+
+//test Foursquare API
+function testGetFourSquare() {
+  axios.get(API_URL + "/venues/explore", {
+    params: {
+      "client_id": CLIENT_ID,
+      "client_secret": CLIENT_SECRET,
+      "v": '20192609', // v for is the version
+      "limit": 50, // limit is how many results returned
+      "ll": '1.2933,103.7831', // latitude/longtitude
+      "query": '', // what we are searching for
+      "categoryId": '4d4b7105d754a06374d81259, 4bf58dd8d48988d16e941735, 4bf58dd8d48988d120951735, 4bf58dd8d48988d16d941735, 4bf58dd8d48988d128941735'
+    }
+  }).then(function (response) {
+    console.log(response.data.response.groups[0].items);
+  })
+}
+
 function getFastFood() {
   axios.get(API_URL + "/venues/search", {
     params: {
@@ -13,7 +32,7 @@ function getFastFood() {
       "client_secret": CLIENT_SECRET,
       "v": '20192609', // v for is the version
       "limit": 50, // limit is how many results returned
-      "center": [103.8198,1.3521] , // longtitude/latitude
+      "center": [103.8198,1.3521], // longtitude/latitude
       "query":'fast food' // what we are searching for
 
     }
@@ -21,3 +40,7 @@ function getFastFood() {
     console.log(response.data.response.groups[0].items);
   })
 }
+
+/* global variables */
+let map;
+let all_markers = [];
