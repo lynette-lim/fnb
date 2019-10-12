5550a0398f664cacdd6fd521e6d40e1bf424cde7
Ubuntu <ubuntu@ip-172-31-33-33.ec2.internal>
2019-10-12 14:34:11 +0000

    Added 'back to top' button

--- script.js
+++ script.js
@@ -671,6 +671,10 @@ $(function() {
         //createPopUp(r)
         $("#results").append(mediaObject);
         
+        // if Object.length === 0; {
+        //   alert("Please search for another category");
+        // }
+        
         let popup = new mapboxgl.Popup({
           offset: 10
         });
@@ -693,14 +697,15 @@ $(function() {
 
 
 $(document).on('click', '.redirectmarker', function() {
-  //console.log(all_markers[$(this).text()[0] -1]);
+  // console.log(all_markers[$(this).text()[0] -1]);
   for(let m of all_markers) 
     if(m.getPopup().isOpen() == true)
       m.togglePopup();
   let marker = all_markers[$(this).text()[0] - 1];
+  
   map.flyTo({
     center: marker._lngLat,
-    zoom: 20
+    zoom: 18
   })
   marker.togglePopup();
 });
@@ -724,3 +729,66 @@ $(document).on('click', '.redirectmarker', function() {
 
       all_markers = [];
 })
+
+
+
+
+
+
+
+
+
+
+
+const backToTopButton = document.querySelector("#back-to-top-btn");
+
+window.addEventListener("scroll", scrollFunction);
+
+function scrollFunction() {
+  if (window.pageYOffset > 300) { // Show backToTopButton
+    if(!backToTopButton.classList.contains("btnEntrance")) {
+      backToTopButton.classList.remove("btnExit");
+      backToTopButton.classList.add("btnEntrance");
+      backToTopButton.style.display = "block";
+    }
+  }
+  else { // Hide backToTopButton
+    if(backToTopButton.classList.contains("btnEntrance")) {
+      backToTopButton.classList.remove("btnEntrance");
+      backToTopButton.classList.add("btnExit");
+      setTimeout(function() {
+        backToTopButton.style.display = "none";
+      }, 250);
+    }
+  }
+}
+
+backToTopButton.addEventListener("click", smoothScrollBackToTop);
+
+// function backToTop() {
+//   window.scrollTo(0, 0);
+// }
+
+function smoothScrollBackToTop() {
+  const targetPosition = 0;
+  const startPosition = window.pageYOffset;
+  const distance = targetPosition - startPosition;
+  const duration = 750;
+  let start = null;
+  
+  window.requestAnimationFrame(step);
+
+  function step(timestamp) {
+    if (!start) start = timestamp;
+    const progress = timestamp - start;
+    window.scrollTo(0, easeInOutCubic(progress, startPosition, distance, duration));
+    if (progress < duration) window.requestAnimationFrame(step);
+  }
+}
+
+function easeInOutCubic(t, b, c, d) {
+	t /= d/2;
+	if (t < 1) return c/2*t*t*t + b;
+	t -= 2;
+	return c/2*(t*t*t + 2) + b;
+};
